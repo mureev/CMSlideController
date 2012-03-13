fbc48cf898dc5014119ecb0f046d803c03a03a28
Constantine Mureev <mureev@gmail.com>
2012-03-13 14:03:13 +0400

    add base classes

---
+++ Source/CMTabBar.m
@@ -0,0 +1,30 @@
+//
+//  CMTabBar.m
+//
+//  Created by Constantine Mureev on 13.03.12.
+//  Copyright (c) 2012 Team Force LLC. All rights reserved.
+//
+
+#import "CMTabBar.h"
+
+@implementation CMTabBar
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
