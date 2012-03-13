1da00583e68e969de524011deca3a93ae80b655a
Constantine Mureev <mureev@gmail.com>
2012-03-13 13:57:18 +0400

    add base classes

---
+++ Source/CMTabBarController.h
@@ -0,0 +1,12 @@
+//
+//  CMTabBarController.h
+//
+//  Created by Constantine Mureev on 13.03.12.
+//  Copyright (c) 2012 Team Force LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMTabBarController : UIViewController
+
+@end
---
+++ Source/CMTabBarController.m
@@ -0,0 +1,42 @@
+//
+//  CMTabBarController.m
+//
+//  Created by Constantine Mureev on 13.03.12.
+//  Copyright (c) 2012 Team Force LLC. All rights reserved.
+//
+
+#import "CMTabBarController.h"
+
+@interface CMTabBarController ()
+
+@end
+
+@implementation CMTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
