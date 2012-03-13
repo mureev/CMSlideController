9c3517310d8df16eac07f2acd78bdb0b9dc83b48
Constantine Mureev <mureev@gmail.com>
2012-03-13 13:39:27 +0400

    add sample project

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Demo CMTabBarController
+//
+//  Created by Constantine Mureev on 13.03.12.
+//  Copyright (c) 2012 Team Force LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
