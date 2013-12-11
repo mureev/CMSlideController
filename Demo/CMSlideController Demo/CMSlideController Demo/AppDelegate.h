1e025917707ad5b1e3996cf7457fa136eeb5039a
Constantine Mureev <mureev@gmail.com>
2013-12-11 11:16:44 +0400

    Create new demo project

---
+++ Demo/CMSlideController Demo/CMSlideController Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CMSlideController Demo
+//
+//  Created by Constantine Mureev on 11/12/13.
+//  Copyright (c) 2013 Constantine Mureev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Demo/CMSlideController Demo/CMSlideController Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CMSlideController Demo
+//
+//  Created by Constantine Mureev on 11/12/13.
+//  Copyright (c) 2013 Constantine Mureev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
