ecd95b037383b5180371f33ca42e1b8ce108293f
Constantine Mureev <mureev@gmail.com>
2013-12-11 11:20:01 +0400

    add blank source files

---
+++ Source/CMSlideController.h
@@ -0,0 +1,12 @@
+//
+//  CMSlideController.h
+//
+//  Created by Constantine Mureev on 11/12/13.
+//  Copyright (c) 2013 Constantine Mureev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CMSlideController : UIViewController
+
+@end
---
+++ Source/CMSlideController.m
@@ -0,0 +1,37 @@
+//
+//  CMSlideController.m
+//
+//  Created by Constantine Mureev on 11/12/13.
+//  Copyright (c) 2013 Constantine Mureev. All rights reserved.
+//
+
+#import "CMSlideController.h"
+
+@interface CMSlideController ()
+
+@end
+
+@implementation CMSlideController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
